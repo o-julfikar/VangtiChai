cd9f0cdfa485ad0cfd4c84a3a1fc88282a4cb6d8
Mohammed Julfikar Ali Mahbub <talk2zulfikar@gmail.com>
2021-11-08 08:39:27 +0600

    Fixed minor bugs.

--- app/src/main/java/com/zulfikar/vangtichai/MainActivity.java
+++ app/src/main/java/com/zulfikar/vangtichai/MainActivity.java
@@ -1,8 +1,6 @@
 package com.zulfikar.vangtichai;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txt500Val, txt100Val, txt50Val, txt20Val, txt10Val, txt5Val, txt2Val, txt1Val, txtTaka;
     private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnClear;
+    private Toast limitToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        limitToast = Toast.makeText(MainActivity.this, "Maximum amount is 4999999", Toast.LENGTH_SHORT);
 
         txt500Val = findViewById(R.id.txt500val);
         txt100Val = findViewById(R.id.txt100val);
@@ -43,70 +43,67 @@ public class MainActivity extends AppCompatActivity {
         btn0 = findViewById(R.id.btn0);
         btnClear = findViewById(R.id.btnClear);
 
-        for (Button btn : new Button[]{
-                btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0}) {
-            btn.setOnClickListener(v -> {
-                String taka = txtTaka.getText().toString().concat(btn.getText().toString());
-                int value = Integer.parseInt("0" + taka.substring(6));
-                if (value > 4999999) {
-                    Toast.makeText(MainActivity.this, "Maximum amount is 4999999", Toast.LENGTH_SHORT).show();
-                } else {
-                    txtTaka.setText(taka);
-                    txt500Val.setText(String.valueOf(value / 500));
-                    txt100Val.setText(String.valueOf(value % 500 / 100));
-                    txt50Val.setText(String.valueOf(value % 500 % 100 / 50));
-                    txt20Val.setText(String.valueOf(value % 500 % 100 % 50 / 20));
-                    txt10Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 / 10));
-                    txt5Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 / 5));
-                    txt2Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 % 5 / 2));
-                    txt1Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 % 5 % 2));
-                }
-            });
-        }
-
-        btnClear.setOnClickListener(this::btnClearOnClick);
-        btnClear.setOnLongClickListener(this::btnClearOnLongClick);
+        addButtonClickListener();
         recover(savedInstanceState);
+    }
+
+    private int getTaka(String taka) {
+        return Integer.parseInt("0" + taka.substring(6));
+    }
+
+    private void vangtiChai(int value) {
+        txt500Val.setText(String.valueOf(value / 500));
+        txt100Val.setText(String.valueOf(value % 500 / 100));
+        txt50Val.setText(String.valueOf(value % 500 % 100 / 50));
+        txt20Val.setText(String.valueOf(value % 500 % 100 % 50 / 20));
+        txt10Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 / 10));
+        txt5Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 / 5));
+        txt2Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 % 5 / 2));
+        txt1Val.setText(String.valueOf(value % 500 % 100 % 50 % 20 % 10 % 5 % 2));
     }
 
     private void btnClearOnClick(View view) {
         String takaText = txtTaka.getText().toString();
         if (!takaText.equals("Taka: ")) {
             txtTaka.setText(takaText.substring(0, takaText.length() - 1));
+            vangtiChai(getTaka(txtTaka.getText().toString()));
         }
     }
 
     private boolean btnClearOnLongClick(View view) {
-        txtTaka.setText("Taka: ");
+        txtTaka.setText(R.string.taka);
+        vangtiChai(0);
         return true;
+    }
+
+    private void addButtonClickListener() {
+        for (Button btn : new Button[]{
+                btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0}) {
+            btn.setOnClickListener(v -> {
+                String taka = txtTaka.getText().toString().concat(btn.getText().toString());
+                int value = getTaka(taka);
+                if (value > 4999999) {
+                    limitToast.show();
+                } else {
+                    txtTaka.setText(R.string.taka + value);
+                    vangtiChai(value);
+                }
+            });
+        }
+        btnClear.setOnClickListener(this::btnClearOnClick);
+        btnClear.setOnLongClickListener(this::btnClearOnLongClick);
     }
 
     private void recover(Bundle savedInstanceState) {
         if (savedInstanceState != null) {
             txtTaka.setText(savedInstanceState.getString("taka"));
-            TextView[] tviews = {txt500Val, txt100Val, txt50Val,
-                    txt20Val, txt10Val, txt5Val, txt2Val, txt1Val};
-            String[] denominations = savedInstanceState.getStringArray("denominations");
-            for (int i = 0; i < tviews.length; i++) {
-                tviews[i].setText(denominations[i]);
-            }
+            vangtiChai(getTaka(txtTaka.getText().toString()));
         }
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putString("taka", txtTaka.getText().toString());
-        outState.putStringArray("denominations", new String[]{
-                txt500Val.getText().toString(),
-                txt100Val.getText().toString(),
-                txt50Val.getText().toString(),
-                txt20Val.getText().toString(),
-                txt10Val.getText().toString(),
-                txt5Val.getText().toString(),
-                txt2Val.getText().toString(),
-                txt1Val.getText().toString(),
-                txtTaka.getText().toString()
-        });
         super.onSaveInstanceState(outState);
     }
 }
